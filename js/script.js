833dcb7ba4a63fa176661d745b34a9bade35ece9
Jared Stanley <jstanley@purestorage.com>
2018-12-05 00:11:25 -0800

    updated w what's live on martian servers

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 //
 var scene,camera,renderer,controls;
-var count = 0;
+var count = 0.05;
 var cameraPosTgt = -100;
 var cameraPosZTgt = -150;
 var light1, light2;
@@ -12,7 +12,7 @@ var vx, vy, vz;
 var loader;
 var curClickTgt;
 var raycaster;
-var motionActive = false;
+var active = true;
 var spinAmt = 0;
 //
 init();
@@ -21,18 +21,18 @@ function init(){
     scene = new THREE.Scene();
     camera = new THREE.PerspectiveCamera( 50, window.innerWidth/window.innerHeight, 0.1, 100000 );
     renderer = new THREE.WebGLRenderer({alpha:true, antialias:true});
-    controls = new THREE.OrbitControls(camera);
+    // controls = new THREE.OrbitControls(camera);
 
   renderer.setSize( window.innerWidth, window.innerHeight );
   document.body.appendChild( renderer.domElement );
 
   camera.position.set(100,900,100);
   //camera.up = new THREE.Vector3(0,1,0);
-  raycaster = new THREE.Raycaster();
+  // raycaster = new THREE.Raycaster();
   initObjects();
   initTextures();
-  initLights();
-  //initInteraction();
+  // initLights();
+  initInteraction();
   // render();
 }
 
@@ -40,69 +40,41 @@ function init(){
 function initObjects(){
   var loader = new THREE.JSONLoader(); // init the loader util
 
-  // loader.load('img/utah.js', function (geometry) {
-    
-  //     earthbox = new THREE.Mesh(geometry,subjMat);
-  //     earthbox.name = "earthbox";
-  //     earthbox.scale.set(4,4,4);
-      // geometry.center();
-  //     earthbox.position.y = -600;
-  //     scene.add(earthbox);
-  // });
-  //
+
   loader.load('img/circle.js', function (geometry) {
-    
+
       base = new THREE.Mesh(geometry,baseMat);
       // base.scale.set(4,4,4);
       geometry.center();
       scene.add(base);
       render();
+
   });
 }
 
 function initTextures(){
     baseMat = new THREE.MeshBasicMaterial({
-    // color:0xffffff, 
+    // color:0xffffff,
     // shading: THREE.FlatShading,
+    side:THREE.DoubleSide,
     transparent: true,
     opacity: 0.9
     });
     baseMat.map = THREE.ImageUtils.loadTexture('img/circle_transp.png');
-    // baseMat = new THREE.MeshPhongMaterial({
-    // color:0xffffff, 
-    // shading: THREE.FlatShading
-    // });
-    // baseMat.map = THREE.ImageUtils.loadTexture('img/map_texture.jpg');
-  
-
-}
-
-function initLights(){
-  light1 = new THREE.PointLight(0xffffff);
-  light1.position.set(-2133,1111,5111);
-
-  scene.add(light1);
-
-  light2 = new THREE.PointLight(0xffffff);
-  light2.position.set(1500,1470,2310);
-
-  scene.add(light2);
-
 
 }
 
 //
 function render() {
-  requestAnimationFrame( render );
+    requestAnimationFrame( render );
+    update();
+    renderer.render(scene, camera);
 
-  update();
-
-  renderer.render(scene, camera);
 };
 
 function initInteraction(){
     window.addEventListener('mousedown', function(e){
-        e.preventDefault(); 
+        e.preventDefault();
         var cx = e.clientX;
         var cy = e.clientY;
         mouseDown(e, cx, cy);
@@ -124,71 +96,37 @@ function initInteraction(){
       e.preventDefault();
       mouseUp(e);
     }, false);
-  
+
 
 }
 
 function mouseDown(e, cx, cy){
-  // console.log("mouseDown", cx, cy);
-  curClickTgt = getRaycastObj(cx, cy);
-  if(curClickTgt){
-    // console.log(curClickTgt);
-    spinearthbox(curClickTgt);
-  }
+  active=false;
+  console.log(active);
 }
 
 //
 function mouseUp(e){
-  if(curClickTgt){
-    // deActivateItem(curClickTgt);
-  }  
+
+  active=true;
+  console.log(active);
 }
 
-function spinearthbox(tgt){
-  motionActive=true;
-}
+
 
 function update(){
-  // base.rotation.y+=0.755699999999996;
-  base.rotation.y+=count;
-  count+=.0003;
-  // count+=Math.PI;
-  // controls.update();
+  if(active){
+    var inc = Math.PI*.00011
+    // base.rotation.y+=0.755699999999996;
+    base.rotation.y-=count;
+    // base.rotation.x+=count/10;
+    count+=inc;
+  }
+
   updateCamera();
 }
 
-function checkTarget(rot){
-  var deg = Math.abs(Math.floor(toDeg(rot)%360));
-  console.log((deg%12));
-}
 
 function updateCamera(){
       camera.lookAt(new THREE.Vector3(0,0,0));
-}
-
-function getRaycastObj(cx,cy){
-  var mouseVect = {
-  x: 2 * (cx / window.innerWidth) - 1,
-  y: 1- 2 * (cy / window.innerHeight)};
-  raycaster.setFromCamera(mouseVect, camera);
-  var intersects = raycaster.intersectObjects([earthbox]);
-    if(intersects.length>0){
-      var obj = intersects[0].object;
-      return obj;
-    }else{
-      return null;
-    }
-}
-
-function toRad(deg){
-  return deg*Math.PI/180;
-}
-
-function toDeg(rad){
-  return rad*180/Math.PI;
-}
-
-function randNum(n){
-  var p = ((Math.random() + Math.random() + Math.random() + Math.random() + Math.random() + Math.random()) - 3) / 3;
-  return p*n;
 }
